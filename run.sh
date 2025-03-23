15136be058d457fcafe12863ba6eee699e2a82bb
TheDogHusky <contact@classydev.fr>
2025-03-23 17:14:46 +0100

    chore: bump version and remove unused variable from run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 APPID="moe.nyarchlinux.updater"
-BUNDLENAME="nyarchupdater.flatpak"
 git add *
 git commit -m "test"
 flatpak-builder --install --user --force-clean flatpak-app "$APPID".json
