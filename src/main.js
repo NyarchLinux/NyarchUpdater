e643302e7266323d0b4a7090b15d0720e181fae7
TheDogHusky <contact@classydev.fr>
2025-03-27 21:28:41 +0100

    chore: bump version

--- src/main.js
+++ src/main.js
@@ -40,7 +40,7 @@ export const NyarchupdaterApplication = GObject.registerClass(
             this.add_action(quitAction);
             this.set_accels_for_action('app.quit', ['<primary>q']);
 
-            this.set_version("0.1.7");
+            this.set_version("0.1.8");
 
             const showAboutAction = new Gio.SimpleAction({ name: 'about' });
             showAboutAction.connect('activate', () => {
