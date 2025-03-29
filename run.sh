17777d6ee8250bed13ee7630f6fe8ae5440aa584
TheDogHusky <contact@classydev.fr>
2025-03-29 18:55:28 +0100

    feat: update run script to uninstall the app on the user
    Fixes the fact that after running this script, the app stays installed in the user scope, making it impossible to self update, and making you run the dev version each time you run the application.

--- run.sh
+++ run.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 APPID="moe.nyarchlinux.updater"
+
+# Function to handle cleanup
+cleanup() {
+    echo "Cleaning up..."
+    flatpak remove --user $APPID --noninteractive --delete-data
+}
+
+# Trap SIGINT (Ctrl + C) and call the cleanup function
+trap 'cleanup; exit' SIGINT
+
 git add *
 git commit -m "test"
 flatpak-builder --install --user --force-clean flatpak-app "$APPID".json
 git reset --soft HEAD~1
+
+# Run the flatpak application
 flatpak run $APPID
+
+# Call the cleanup function after the flatpak application exits
+cleanup
